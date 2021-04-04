14d88a283d62a4e307524524a42037246ca987a8
pedro oste <pedstarpedstar@yahoo.com.au>
2021-04-04 15:59:29 +1000

    Code cleanup and bug squashes
    A number of updates to the code has been made ensuring a cleaner look,
    as well as elminiating bugs
    
    Code cleanup:
    - Seperated the two classes into seperate files
    - Included more comments
    - Code didnt represent true OOP form (incorrect access of private and
      public attributes)
    Bugs:
    - The program would crash when faced with a character that was not
      within the key (outside of alphabet) this would include spaces,
      numbers etc.
    
      to fix this i ensured that the message was lowercase, and any
      characters that was not within the key was skipped, and the normal
      character was written in the encrypted message

--- CipherEncryption.java
+++ CipherEncryption.java
@@ -1,103 +1,27 @@
+/*
+Author: Pedro Oste
+Email: pedro.ost3@gmail.com
+Encrypts a user input based on a Monoalphabetic Cipher
+*/
+
+
 //import statements
 import java.util.*;
 
 
+//main class
 class CipherEncryption{
+    
+    //scanner to gain user input
     public static Scanner scan = new Scanner(System.in);
     public static void main(String[] args){
-        System.out.println("Compiled");
-        System.out.println("Please enter the message you would like to encrypt: ");
+        //getting user input
+        System.out.print("Please enter the message you would like to encrypt: ");
+        
+        //creating encyption object, jey and cipher using the input
         Encryption encrypt = new Encryption(scan.nextLine());
         encrypt.createKey();
         encrypt.createCipher();
     }
 }
 
-
-class Encryption{
-    private String message;
-    private String cipher;
-    private ArrayList<Character> alpha = new ArrayList<Character>();        //alphabet
-    private ArrayList<Character> key = new ArrayList<Character>();
-
-    //Constructor
-    Encryption(String message){
-        this.message = message;
-        
-        //creating arraylist of alphabet
-        Character[] temp = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        Collections.addAll(alpha, temp);
-    }
-
-    //getters and setters
-
-    public String getMessage() {
-        return this.message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public ArrayList<Character> getAlpha() {
-        return this.alpha;
-    }
-
-    public void setAlpha(ArrayList<Character> alpha) {
-        this.alpha = alpha;
-    }
-
-    public ArrayList<Character> getKey() {
-        return this.key;
-    }
-
-    public void setKey(ArrayList<Character> key) {
-        this.key = key;
-    }
-    
-
-
-    //creates key for encryption to use
-    public void createKey(){
-        //creating a temporary array of letter
-        ArrayList<Character> temp = (ArrayList<Character>) alpha.clone();
-
-        //creating random number object
-        Random rand = new Random();
-        int upperbound = 26;
-
-        //assigning key
-        for (int i = 0; i < alpha.size(); i++){
-            //getting random number
-            int randNum = rand.nextInt(upperbound);          
-            //getting random letter
-            Character letter = temp.get(randNum);
-            //removing letter
-            temp.remove(randNum);
-            //reducing random size
-            upperbound --;
-            //adding letter to key
-            key.add(letter);
-
-        }
-
-        System.out.printf("Key created, %s", getKey());
-    }
-
-    //creates cipher text based on encryption key
-    public void createCipher(){
-        /*Stumbles on non letter, spaces and full stops */
-        for (int i = 0; i < message.length() ; i++){
-            //getting letter
-            Character letter = message.charAt(i);
-            System.out.println(alpha.indexOf(letter));
-            //getting cipher letter
-            Character cLetter = key.get(alpha.indexOf(letter));
-            //adding letter to cipher text
-            cipher += Character.toString(cLetter);
-        }
-
-        System.out.printf("Cipher Text %s", cipher);
-    }
-
-}
---
+++ Encryption.java
@@ -0,0 +1,126 @@
+//import statements
+import java.util.*;
+
+class Encryption{
+    //user message
+    private String message;
+    //encrypted user message using cipher
+    private String cipher;
+    //alphabet arraylist
+    private static ArrayList<Character> alpha = new ArrayList<>(
+        Arrays.asList('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'));
+    //Key generated to ecnrypt message
+    private ArrayList<Character> key = new ArrayList<>();
+
+    //random object (used to generate key)
+    public static Random rand = new Random();
+
+    //Constructor
+    Encryption(String message){
+        this.message = message;
+    }
+
+    //getters and setters
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        //assigning the message in lower case form        
+        this.message = message.toLowerCase();
+    }
+
+    public String getCipher(){
+        return this.cipher;
+    }
+
+    public void setCipher(String cipher){
+        this.cipher = cipher;
+    }
+
+    public ArrayList<Character> getAlpha() {
+        return this.alpha;
+    }
+
+    public ArrayList<Character> getKey() {
+        return this.key;
+    }
+
+    public void setKey(ArrayList<Character> key) {
+        this.key = key;
+    }
+
+    
+
+
+    //creates key for encryption to use
+    public void createKey(){
+
+        //creating a temporary array of alphabet
+        ArrayList<Character> temp = (ArrayList<Character>) alpha.clone();
+
+        //creating a local key
+        ArrayList<Character> localKey = new ArrayList<>();
+
+        //creating random upperbound
+        int upperbound = 26;
+
+        //assigning key
+        for (int i = 0; i < alpha.size(); i++){
+            //getting random number
+            int randNum = rand.nextInt(upperbound);          
+            //getting random letter
+            Character letter = temp.get(randNum);
+            //removing letter
+            temp.remove(randNum);
+            //reducing random size
+            upperbound --;
+            //adding letter to key
+            localKey.add(letter);
+
+        }
+        
+        //assining key to object
+        setKey(localKey);
+        
+        //displaying alphabet
+        System.out.printf("Alphabet:\n%s\n", getAlpha());
+
+        //displaying key 
+        System.out.printf("Key created:\n%s\n", getKey());
+
+    }
+
+    //creates cipher text based on encryption key
+    public void createCipher(){
+        
+        //creating local cipher
+        String localCipher = "";
+
+        /*Stumbles on non letter, spaces and full stops */
+        for (int i = 0; i < message.length() ; i++){
+            
+            //getting character to encrpyt
+            Character letter = message.charAt(i);
+
+            //determining if the character presented is within the key (this will skip any special characters and full stops)
+            int charIndex = getAlpha().indexOf(letter);
+            Character cLetter;
+
+            if(charIndex != -1){
+                //getting cipher letter
+                cLetter = key.get(charIndex);
+            }
+            else{
+                cLetter = letter;
+            }
+            
+            //adding letter to cipher text
+            localCipher += Character.toString(cLetter);
+        }
+
+        System.out.printf("Cipher Text: %s", localCipher);
+    }
+
+}
